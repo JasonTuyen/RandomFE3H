e17bac2ad36f3ddb3a460c884be80ca9618c1b87
Jason Tuyen <34723311+JasonTuyen@users.noreply.github.com>
2020-07-18 02:38:14 -0700

    Create GUI.py

---
+++ GUI.py
@@ -0,0 +1,17 @@
+import tkinter as *
+
+window = Tk()
+
+l1=label(window, text="Title")
+l1.grid(row=0, column=0)
+
+l1=label(window, text="Title")
+l1.grid(row=0, column=0)
+
+l1=label(window, text="Title")
+l1.grid(row=0, column=0)
+
+l1=label(window, text="Title")
+l1.grid(row=0, column=0)
+
+window.mainloop()
